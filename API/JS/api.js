aff5a8c5e9a7fdc23ea90a59b3f5ffa44250ff55
MinhajulAkib <00akibswe@gmail.com>
2021-08-29 23:59:46 +0600

    Load More Data, More API, Send Data To Function

--- API/JS/api.js
+++ API/JS/api.js
@@ -3,3 +3,19 @@ function loadData(){
   .then(response => response.json())
   .then(json => console.log(json))
 }
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then(data => displayUsers(data))
+ }
+
+ function loadPost(){
+     fetch('https://jsonplaceholder.typicode.com/posts')
+     .then(response => response.json())
+     .then(data => console.log(data))
+ }
+ loadPost();
+ function displayUsers(data){
+     console.log(data)
+ }
