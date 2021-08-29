ffc4183274e30ab1c04aac45e7d388b20070840e
MinhajulAkib <00akibswe@gmail.com>
2021-08-30 00:39:36 +0600

    Dynamically Display Loaded Data On Your Website

--- API/JS/api.js
+++ API/JS/api.js
@@ -3,19 +3,29 @@ function loadData(){
   .then(response => response.json())
   .then(json => console.log(json))
 }
+function loadPost(){
+     fetch('https://jsonplaceholder.typicode.com/posts')
+     .then(response => response.json())
+     .then(data => displayPost(data))
+ }
+function displayPost(data){
+     const ol = document.getElementById('load');
+     console.log(data);
+     for(const user of data){
+         console.log(user);
+         const li = document.createElement('li');
+        li.innerText = `Id: ${user.id}, Title: ${user.title}`;
+        ol.appendChild(li);
+     }
+ }
 
+
+//Dynamically Display Loaded Data On Your Website
 function loadUsers(){
     fetch('https://jsonplaceholder.typicode.com/users')
    .then(response => response.json())
    .then(data => displayUsers(data))
  }
-
- function loadPost(){
-     fetch('https://jsonplaceholder.typicode.com/posts')
-     .then(response => response.json())
-     .then(data => console.log(data))
- }
-
  function displayUsers(data){
      const ul = document.getElementById('users');
      console.log(data);
