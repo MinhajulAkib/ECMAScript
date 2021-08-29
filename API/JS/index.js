b8026e33427f9708658346371bf8477e33950da4
MinhajulAkib <00akibswe@gmail.com>
2021-08-29 23:17:01 +0600

    Json, Json Structure, Parse, Stringify

--- API/JS/index.js
+++ API/JS/index.js
@@ -1,2 +1,31 @@
+//javascript object notation
+//JSON => 
 const user = { id: 11, name: 'gorib amir', job: 'actor' };
-console.log(user);
+const stringified= JSON.stringify(user);
+// console.log(stringified);
+// console.log(user);
+
+
+const shop = {
+    name: 'alia store',
+    address: 'ranbir road',
+    profit: 15000,
+    products: [ 'laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'alia bhatt',
+        profession: 'actor'
+    },
+    isExpensive: false
+}
+
+//convert object to stringified
+//using stringified
+const shopJson = JSON.stringify(shop);
+//console.log(shop);
+console.log(shopJson);
+
+
+//convert stringifird to Object
+//using parse
+const convert = JSON.parse(shopJson);
+console.log(convert);
