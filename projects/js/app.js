e070d3bf7f1f131a457c67f172c8cb58a7ad372b
MinhajulAkib <00akibswe@gmail.com>
2021-09-04 10:01:01 +0600

    Display temperature, city, dynamic weather icon

--- projects/js/app.js
+++ projects/js/app.js
@@ -8,6 +8,17 @@ const searchTemperature = () => {
     .then(data => displayTemperature(data));
 }
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperature = temperature => {
-      console.log(temperature);
+    setInnerText('city',temperature.name);
+    setInnerText('temperature',temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    
+    //set icons
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 }
